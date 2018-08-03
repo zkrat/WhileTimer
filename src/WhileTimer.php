160ec3ccf7280135d75f2af50a08c15e1af7ffc6
zkrat <hajek@zkrat.net>
2018-08-03 10:42:41 +0200

    same length cycle

--- src/WhileTimer.php
+++ src/WhileTimer.php
@@ -14,13 +14,27 @@ class WhileTimer {
 	private static $time=null;
 	private static $counter=0;
 
+
+	private static $lastMicroTime=null;
+	private static $microTime=null;
+	const MICROSECONDS_IN_SECONDS=1000000;
+
 	/**
 	 * @param int $totalTime total time in secodns
 	 * @param float $timeCycle sleep (s) time of each cycle
 	 */
 	public static function sleep($totalTime,$timeCycle  ):bool {
-		usleep(round($timeCycle*1000000));
-		return self::inCycle(round($totalTime*1000000));
+		$microseconds=$timeCycle*WhileTimer::MICROSECONDS_IN_SECONDS;
+		self::delay($microseconds);
+		return self::inCycle($totalTime*WhileTimer::MICROSECONDS_IN_SECONDS,$timeCycle);
+
+	}
+
+	private static function reset(  ) {
+		self::$time=null;
+		self::$counter=0;
+		self::$lastMicroTime=null;
+		self::$microTime=null;
 
 	}
 
@@ -29,15 +43,33 @@ class WhileTimer {
 	 * @param int $timeCycle sleep  time of each cycle (microseconds)
 	 */
 	public static function usleep($totalTime,$timeCycle  ):bool {
-		usleep($timeCycle);
-		return self::inCycle($totalTime);
+		self::delay($timeCycle);
+		return self::inCycle($totalTime,$timeCycle);
+
+	}
+
+	private static function delay($timeCycle){
+		self::$microTime=microtime(true)*WhileTimer::MICROSECONDS_IN_SECONDS;
+		if (!is_null(self::$lastMicroTime)){
+
+			$delay =$timeCycle-(self::$microTime-self::$lastMicroTime);
+
+			if ($delay>0){
+				usleep($delay);
+			}
+			self::$microTime=microtime(true)*WhileTimer::MICROSECONDS_IN_SECONDS;
+
+		}
+		self::$lastMicroTime=self::$microTime;
+
 
 	}
 
 
-	private static function inCycle($totalTime  ):bool {
+	private static function inCycle($totalTime ,$timeCycle ):bool {
 		self::$counter++;
-		$microtime = microtime(true)*1000000;
+		$microtime = microtime(true)*WhileTimer::MICROSECONDS_IN_SECONDS;
+		$timeCycle *=WhileTimer::MICROSECONDS_IN_SECONDS;
 		if(is_null(self::$time))
 			self::$time=$microtime;
 
@@ -46,7 +78,11 @@ class WhileTimer {
 			self::$time=null;
 			self::$counter=0;
 		}
-		return $second<$totalTime;
+		$bool=$second+$timeCycle<$totalTime;
+
+		if (!$bool)
+			self::reset();
+		return $bool;
 	}
 
 	/**
